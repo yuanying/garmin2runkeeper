c9ef30bf0a098575c216e55904c0c95993fac7ba
yuanying <yuanying@fraction.jp>
2012-05-16 17:46:34 +0900

    ログ出力を追加。

--- lib/synchronizer.rb
+++ lib/synchronizer.rb
@@ -4,6 +4,7 @@ module Synchronizer
 
     User.excludes(:garmin_id => nil).all.each do |user|
       begin
+        puts "#{user.uid}: #{user.runkeeper_auth.info.name}"
         user.sync(exporter)
       rescue => ex
         p ex
